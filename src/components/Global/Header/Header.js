7221f07443df0b1fd398bff700394df9b74d8a37
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2022-05-06 11:19:09 +0600

    added header component

--- src/components/Global/Header/Header.js
+++ src/components/Global/Header/Header.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import Customlink from '../../../Hooks/Customlink';
 
 const Header = () => {
     return (
         <div>
-            asdsa
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+                <Container className=''>
+                    <Navbar.Brand >
+                        <Customlink to={'/'}> <img width={150} src='https://res.cloudinary.com/tusar/image/upload/v1650267511/travel/touraholic_y37ysx.png' alt="" /> </Customlink>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Customlink to={'/'}>Home</Customlink>
+                            <Customlink to={'/add-product'}>Add Products</Customlink>
+
+
+                        </Nav>
+                        <Nav>
+
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };

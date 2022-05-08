7eb325908256db8523fa0233a6ca8db40dcc3b47
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2022-05-08 13:34:37 +0600

    blog update

--- src/components/Pages/Blog/Blog.js
+++ src/components/Pages/Blog/Blog.js
@@ -8,37 +8,35 @@ const Blog = () => {
             <Pagetitle title="BLOG" subtitle="Assignments Questions and answers"></Pagetitle>
             <Container className='py-5'>
 
-                <div className="qa_Wrapper">
+                <div className="qa_Wrapper p-4 my-4 shadow">
                     <h4 className="question ">
-                        Q1: Difference between javascript and nodejs
+                        Q1: Differences between sql and nosql databases.
                     </h4>
                     <hr></hr>
-                    <div className="answers">
-                        <p>The Context API is a React structure that allows you to exchange unique details and helps in prop-drilling resolution from all levels of your application.</p>
-                        <p>The React Context API allows a React app to generate global variables that can be passed around. This is an alternative to "prop drilling," which involves passing props from grandparent to child to parent, and so on. Context is also marketed as a simpler, lighter approach to Redux state management.</p>
+                    <div className="answers ">
+                        <div class="table-responsive">
+                            <table class="table table-bordered">
+                                <thead><tr><th>SQL Database</th><th>NOSQL Database</th></tr></thead>
+                                <tbody>
+                                    <tr><td>SQL full form is Structured Query Language.</td><td>NOSQL full form is Not Only SQL.</td></tr>
+                                    <tr><td>SQL database is horizontally scalable database.</td><td> NoSQL database is vertically scalable database.</td></tr>
+                                    <tr><td>SQL database is basically table based database</td><td>NoSQL database is document based database.</td></tr>
+                                    <tr><td>Example: <b> MySQL, Microsoft SQL Server, Oracle Database, PostgreSQL</b></td><td>Example: <b>MongoDB</b></td></tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-                <div className="qa_Wrapper">
+                <div className="qa_Wrapper p-4 my-4 shadow">
                     <h4 className="question">
-                        Q2: Differences between sql and nosql databases.
+                        Q2: Difference between javascript and nodejs
                     </h4>
                     <hr></hr>
                     <div className="answers">
-                        <p>A semantic element's defining feature is that it clearly communicated its meaning to both the developer and the browser. These elements define its content really very clearly.</p>
-                        <p>Example of some,
-
-                            <ul>
-                                <li> header: It defines a header for a web page.</li>
-                                <li>nav: It defines a container for navigation links.</li>
-                                <li>section: This defines a section in a web page.</li>
-                                <li>article: This element contains the main part, containing information about the web page.</li>
-                                <li>aside: The aside content is often placed as a sidebar in a document.</li>
-                                <li>footer: It defines a footer for a document or a section.</li>
-                            </ul>
-                        </p>
+                        <div class="table-responsive"><table class="table table-bordered"><thead><tr><th>Javascript</th><th>Nodejs</th></tr></thead><tbody><tr><td>JavaScript is a  programming language that runs in browser.</td><td>Node JS is a javascript programming language interpreter or running environment.</td></tr><tr><td>JavaScript is used for any client-side activity of a web application.</td><td> Nodejs is used for non-blocking operation of a operating system.</td></tr><tr><td>JavaScript can run on many engines like Spider monkey (FireFox), JavaScript Core (Safari), V8 (Google Chrome)</td><td>NodeJs can run in only V8(used by google chrome browser) engine.</td></tr></tbody></table></div>
                     </div>
                 </div>
-                <div className="qa_Wrapper">
+                <div className="qa_Wrapper p-4 my-4 shadow">
                     <h4 className="question">
                         Q3: What is the purpose of jwt and how does it work
                     </h4>
@@ -49,24 +47,20 @@ const Blog = () => {
                         </p>
                     </div>
                 </div>
-                <div className="qa_Wrapper">
+                <div className="qa_Wrapper p-4 my-4 shadow">
                     <h4 className="question">
                         Q2: When should you use nodejs and when should you use mongodb
                     </h4>
                     <hr></hr>
                     <div className="answers">
-                        <p>A semantic element's defining feature is that it clearly communicated its meaning to both the developer and the browser. These elements define its content really very clearly.</p>
-                        <p>Example of some,
 
-                            <ul>
-                                <li> header: It defines a header for a web page.</li>
-                                <li>nav: It defines a container for navigation links.</li>
-                                <li>section: This defines a section in a web page.</li>
-                                <li>article: This element contains the main part, containing information about the web page.</li>
-                                <li>aside: The aside content is often placed as a sidebar in a document.</li>
-                                <li>footer: It defines a footer for a document or a section.</li>
-                            </ul>
-                        </p>
+
+
+                        <ul>
+                            <li> Nodejs is a tool for creating non-blocking event-driven servers. Nodejs is primarily used in real-time applications such as online games and chat services. It's used to make RestAPI for websites.</li>
+                            <li>MongoDB is a database that is not stored in a relational database. So its said to be a NOSQL database. It's used to store information in the form of documents, such as json. This database is best for data that doesn't fit into a relational database schema.</li>
+                        </ul>
+
                     </div>
                 </div>
             </Container>
